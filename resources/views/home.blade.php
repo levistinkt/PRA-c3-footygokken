023a377e92c2dfdae773081a9540e0fe994a5e7d
levistinkt <sdestinkerd@gmail.com>
2024-11-28 13:57:38 +0100

    mainpage met layout
    >mainpage toegevoegd
    >routing aangepast
    >onnodige files verwijderd
    >layout toegevoegd
    >navbar toegevoegd
    >footer toegevoegd
    >migrations gemaakt. NIET VERGETEN

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.app>
+
+    <h2>Welkom op onze website !</h2>
+
+    <h1>FootyGokken !</h1>
+
+    <p>Welkom op de homepage van FootyGokken. We zijn verheugd dat je hier bent. Op deze site vindt u al uw voetbal informatie op een plek ! Gebruik de navigatie om naar de andere pagina's te gaan.</p>
+
+</x-layouts.app>
